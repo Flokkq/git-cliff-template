8117335f9a89690cc76587c285b87b615c97def2
Flokkq <webcla21@htl-kaindorf.at>
2025-01-02 03:09:26 +0100

    refactor(nix): flake housekeeping
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- flake.nix
+++ flake.nix
@@ -6,20 +6,23 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem(system:
-      let
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [
-                pkgs.git-cliff
-                pkgs.gnupg
-            ];
-          };
-        }
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            git-cliff
+            gnupg
+          ];
+        };
+      }
     );
 }
